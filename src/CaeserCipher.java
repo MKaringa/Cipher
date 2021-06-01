3a86484e78496afd03d8cfa668156ea964617c06
MKaringa <mungaikaringa1@gmail.com>
2021-06-01 10:58:31 +0300

    Create a constructor that accepts message and key as parameters

--- src/CaeserCipher.java
+++ src/CaeserCipher.java
@@ -3,6 +3,11 @@ public class CaeserCipher{
     private String theMessage;
     private int shiftKey;
 
+    public CaeserCipher(String message, int key) {
+        this.theMessage = message;
+        this.shiftKey = key;
+    }
+
 }
 
 
